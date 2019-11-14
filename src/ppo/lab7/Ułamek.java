faa3cd27fe99285166d0e07ff122cd6bc0b92cf3
Radek Piórkowski <r.piorkowski@mimuw.edu.pl>
2019-11-14 13:48:59 +0100

    koniec lab7

---
+++ src/ppo/lab7/Ułamek.java
@@ -0,0 +1,39 @@
+package ppo.lab7;
+
+public class Ułamek {
+    private int licznik, mianownik;
+
+    public Ułamek(int nLicznik, int nMianownik) {
+        licznik = nLicznik;
+        mianownik = nMianownik;
+
+        int M = NWD(licznik, mianownik);
+        licznik /= M;
+        mianownik /= M;
+
+        if (mianownik < 0) {
+            mianownik *= -1;
+            licznik *= -1;
+        }
+    }
+
+    public String toString() {
+        return String.format("(%d/%d)", licznik, mianownik);
+    }
+
+    private static int NWD(int a, int b) {
+        while (b != 0) {
+            int c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+
+    public Ułamek dodaj(Ułamek u2) {
+        return new Ułamek(
+                licznik*u2.mianownik + u2.licznik*mianownik,
+                mianownik * u2.mianownik
+        );
+    }
+}
